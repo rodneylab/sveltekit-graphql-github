4d6aa5df903e0c343fd4b8df1a4c1d7eb6d0988c
Rodney Lab <codesign@rodneylab.com>
2023-11-22 16:20:43 +0000

    style: 🛁 formatting

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,5 +1,5 @@
 // based on https://github.com/tretapey/svelte-pwa/blob/master/public/service-worker.js
-import { build, files, version } from "$service-worker";
+import { build, files, version } from '$service-worker';
 
 const CACHE_NAME = `static-cache-${version}`;
 const to_cache = build.concat(files);
@@ -11,54 +11,54 @@ const to_cache = build.concat(files);
 
 // Using IIFE to provide closure to redefine `self`
 (() => {
-  // This is a little messy, but necessary to force type assertion
-  // Same issue as in TS -> https://github.com/microsoft/TypeScript/issues/14877
-  // prettier-ignore
-  const self =
+	// This is a little messy, but necessary to force type assertion
+	// Same issue as in TS -> https://github.com/microsoft/TypeScript/issues/14877
+	// prettier-ignore
+	const self =
     /** @type {ServiceWorkerGlobalScope} */ (/** @type {unknown} */ (globalThis
       .self));
-  self.addEventListener("install", () => {
-    console.log("[ServiceWorker] Install");
+	self.addEventListener('install', () => {
+		console.log('[ServiceWorker] Install');
 
-    self.waitUntil(
-      caches.open(CACHE_NAME).then((cache) => {
-        console.log("[ServiceWorker] Pre-caching offline page");
-        return cache.addAll(to_cache).then(() => {
-          self.skipWaiting();
-        });
-      }),
-    );
-  });
+		self.waitUntil(
+			caches.open(CACHE_NAME).then((cache) => {
+				console.log('[ServiceWorker] Pre-caching offline page');
+				return cache.addAll(to_cache).then(() => {
+					self.skipWaiting();
+				});
+			}),
+		);
+	});
 
-  self.addEventListener("activate", (event) => {
-    console.log("[ServiceWorker] Activate");
-    // Remove previous cached data from disk
-    event.waitUntil(
-      caches.keys().then(async (keys) =>
-        Promise.all(
-          keys.map((key) => {
-            if (key !== CACHE_NAME) {
-              console.log("[ServiceWorker] Removing old cache", key);
-              return caches.delete(key);
-            }
-          }),
-        )
-      ),
-    );
-    self.clients.claim();
-  });
+	self.addEventListener('activate', (event) => {
+		console.log('[ServiceWorker] Activate');
+		// Remove previous cached data from disk
+		event.waitUntil(
+			caches.keys().then(async (keys) =>
+				Promise.all(
+					keys.map((key) => {
+						if (key !== CACHE_NAME) {
+							console.log('[ServiceWorker] Removing old cache', key);
+							return caches.delete(key);
+						}
+					}),
+				),
+			),
+		);
+		self.clients.claim();
+	});
 
-  self.addEventListener("fetch", (event) => {
-    console.log("[ServiceWorker] Fetch", event.request.url);
-    if (event.request.mode !== "navigate") {
-      return;
-    }
-    event.respondWith(
-      fetch(event.request).catch(() => {
-        return caches.open(CACHE_NAME).then((cache) => {
-          return cache.match("offline.html");
-        });
-      }),
-    );
-  });
+	self.addEventListener('fetch', (event) => {
+		console.log('[ServiceWorker] Fetch', event.request.url);
+		if (event.request.mode !== 'navigate') {
+			return;
+		}
+		event.respondWith(
+			fetch(event.request).catch(() => {
+				return caches.open(CACHE_NAME).then((cache) => {
+					return cache.match('offline.html');
+				});
+			}),
+		);
+	});
 })();
